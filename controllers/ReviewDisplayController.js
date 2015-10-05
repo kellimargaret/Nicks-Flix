054cb204bd482d89ffdea1e7512877694d1577a1
Kelli Margaret <kelli.sommerdyke@gmail.com>
2015-10-05 11:08:08 -0700

    Fix bug with typo in ReviewsDisplayController

---
+++ controllers/ReviewDisplayController.js
@@ -0,0 +1,3 @@
+nicksFlix.controller('ReviewDisplayCtrl', function ReviewDisplayCtrl($scope, $stateParams, ReviewsFactory, UtilitiesFactory) {
+  $scope.review = UtilitiesFactory.findById(ReviewsFactory.reviews, $stateParams.reviewId);
+});
